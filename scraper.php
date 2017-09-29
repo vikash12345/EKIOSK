887b97c695ba2aae2c653c68dbe1ad86183818f5
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-29 09:46:38 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -8,7 +8,8 @@ for($page = 7; $page <9; $page++)
 {
   $LINK =   'http://202.61.43.37:8082/ekioskv2/(S(kq4pwh3tumdqjfrn2fckn3ch))/CaseProfile.aspx?1='.$page;
   $html   =   file_get_html($LINK);
-  echo $html;
+  $caseno =   $html-find("//*[@id='lbl_caseno']",0)->plaintext;
+  echo $caseno.'\n';
 }
 
 //
